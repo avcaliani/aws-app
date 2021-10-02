cd5d375cc65224ecec0926552cdc3890a2f5ae09
Anthony Caliani <avcaliani.it@gmail.com>
2021-10-02 17:37:39 -0300

    [emr-app] Implementing Hudi for trusted pipeline.

--- emr/user-app/src/pipelines/trusted.py
+++ emr/user-app/src/pipelines/trusted.py
@@ -1,28 +1,59 @@
-from pyspark.sql import SparkSession
+import json
 
+from pyspark.sql import functions as f
+
+from src import constants
 from src.utils import log
 
-RAW_PATH = 'raw/users'
-TRUSTED_PATH = 'trusted/users'
+HUDI_TABLE = 'user_score'
+HUDI_KEY_COL = 'user_id'
+HUDI_INCREMENT_COL = 'updated_at'
+HUDI_PARTITION_COL = HUDI_KEY_COL  # 'yy_mm'
 
 
-def run(spark: SparkSession, **kwargs):
-    bucket, dt_partition = kwargs['bucket'], kwargs['dt_partition']
-    raw_file = f'{bucket}/{RAW_PATH}/{dt_partition}'
-    log.info(f'Reading file "{raw_file}"')
+def _read(spark, bucket, exec_date):
+    log.info('=~=~=~=~=~=~=~=~=~= READ =~=~=~=~=~=~=~=~=~=')
+    path = constants.get('raw_path').format(bucket, exec_date)
+    log.info(f'File: {path}')
     df = spark.read \
         .option('header', True) \
         .option('inferSchema', True) \
         .option('sep', ',') \
-        .csv(raw_file) \
+        .csv(path) \
         .cache()
+    log.info(f'Records: {df.count()}')
+    df.printSchema()
+    return df
 
-    log.info(f'Records found: {df.count()}')
+
+def _write(df, bucket, overwrite):
+    log.info('=~=~=~=~=~=~=~=~=~= WRITE =~=~=~=~=~=~=~=~=~=')
+    path = constants.get('trusted_path').format(bucket)
+    hudi_opts = constants.hudi_options(
+        table=HUDI_TABLE,
+        key_col=HUDI_KEY_COL,
+        increment_col=HUDI_INCREMENT_COL,
+        partition_col=HUDI_PARTITION_COL
+    )
+    log.info(f'File: {path}')
+    log.info(f'Overwrite: {overwrite}')
+    log.info(f'Records: {df.count()}')
+    log.info(f'HUDI Opts: {json.dumps(hudi_opts, indent=2, ensure_ascii=False)}')
     df.printSchema()
     df.show()
-
-    log.info(f'Writing to "{bucket}/{TRUSTED_PATH}"')
     df.write \
-        .mode('append') \
-        .partitionBy('user_id') \
-        .parquet(f'{bucket}/{TRUSTED_PATH}')
+        .format("hudi") \
+        .options(**hudi_opts) \
+        .mode('overwrite' if overwrite else 'append') \
+        .save(path)
+    return df
+
+
+def run(spark, **kwargs):
+    bucket, overwrite = kwargs['bucket'], kwargs['overwrite']
+    df = _read(spark, bucket, kwargs['exec_date'])
+    df = df.withColumn('processed_at', f.current_timestamp()) \
+        .withColumn('updated_at', f.to_timestamp('updated_at', 'yyyy-MM-dd HH:mm:ss')) \
+        .withColumn('yy_mm', f.date_format('updated_at', 'yyyy-MM'))
+
+    _write(df, bucket, overwrite)
