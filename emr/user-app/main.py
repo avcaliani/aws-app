1fbb9d9b5c6fe7630390adf999816daa587f2aac
Anthony Caliani <avcaliani.it@gmail.com>
2021-10-02 17:38:28 -0300

    [emr-app] Updating pipelines and spark config.

--- emr/user-app/main.py
+++ emr/user-app/main.py
@@ -3,7 +3,7 @@ from contextlib import contextmanager
 
 from pyspark.sql import SparkSession
 
-from src.pipelines import trusted
+from src.pipelines import trusted, info
 from src.utils import Timer
 from src.utils import log
 
@@ -11,8 +11,9 @@ from src.utils import log
 def get_args() -> Namespace:
     parser = ArgumentParser(description='User Score Ingestion')
     parser.add_argument('pipeline', help='Pipeline name')
-    parser.add_argument('--date', dest='dt_partition', help='Partition date as "yyyy-mm-dd"')
-    parser.add_argument('--bucket', default='nth-dev-datalake', help='S3 Bucket')
+    parser.add_argument('--date', dest='exec_date', help='Partition date as "yyyy-mm-dd"')
+    parser.add_argument('--bucket', default='nth-dev-datalake', help='S3 Bucket name or absolute local path.')
+    parser.add_argument('--overwrite', dest='overwrite', action='store_true', default=False)
     return parser.parse_args()
 
 
@@ -20,15 +21,18 @@ def get_args() -> Namespace:
 def spark_session(pipeline: str):
     spark = SparkSession.builder \
         .appName(f'user-score_{pipeline}') \
+        .config('spark.serializer', 'org.apache.spark.serializer.KryoSerializer') \
         .getOrCreate()
     yield spark
     spark.stop()
 
 
 def run(spark: SparkSession, args: Namespace) -> None:
-    pipeline = args.pipeline.upper()
-    if 'TRUSTED' == pipeline:
-        trusted.run(spark, dt_partition=args.dt_partition, bucket=args.bucket)
+    pipeline = args.pipeline.lower()
+    if 'trusted' == pipeline:
+        trusted.run(spark, exec_date=args.exec_date, bucket=args.bucket, overwrite=args.overwrite)
+    elif 'info' == pipeline:
+        info.run(spark, bucket=args.bucket)
     else:
         raise RuntimeError(f'Pipeline "{pipeline}" does not exist!')
 
